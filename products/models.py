05596137c1ef62a8f8cfe672ea0eb01aa28ca73b
gbronca <gbronca@gmail.com>
2019-12-21 16:06:19 +0000

    created base.html file

--- products/models.py
+++ products/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title       = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
     description = models.TextField(null=True)
-    price       = models.DecimalField(max_digits=8, decimal_places=2)
-    colour      = models.CharField(max_length=20, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    colour = models.CharField(max_length=20, null=True)
+    image = models.FileField(upload_to='products/')
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class Stock(models.Model):
+    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
