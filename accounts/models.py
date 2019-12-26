4f1ec61353e01f41418622615132f7016645768c
gbronca <gbronca@gmail.com>
2019-12-26 15:35:08 +0000

    User address model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Address(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    postcode = models.CharField(max_length=10)
+    house_number = models.CharField('Number', max_length=35)
+    street = models.CharField(max_length=50)
+    town = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.postcode} - {self.house_number}'
